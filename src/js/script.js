e825de7529683d22c0d5a1ff24faf7f96b597006
Sean Chittenden <seanc@groupon.com>
2015-03-09 01:13:08 -0700

    Always use the hardcoded switches in index
    This should be an array, but whatever.

--- src/js/script.js
+++ src/js/script.js
@@ -1,55 +1,7 @@
 (function($) {
   "use strict";
 
-  var saved_switches = ( $.jStorage.get('switches') ? $.jStorage.get('switches') : new Array() );
-
-  if ( saved_switches ) {
-    for ( var i = 0; i < saved_switches.length; i++ ) {
-      addSwitch(saved_switches[i]);
-    }
-  }
-
-  $('#add-switch').on(
-    'click',
-    function(e) {
-      e.preventDefault();
-      var new_ip = prompt('What is the IP address for the new switch?');
-      var new_label = prompt('What would you like to name this switch?');
-
-      if ( new_ip && new_label ) {
-        var new_switch = {label: new_label, ip: new_ip};
-        saved_switches.push( new_switch );
-
-        $.jStorage.set('switches',saved_switches);
-
-        addSwitch(new_switch);
-      }
-    }
-  );
-
-  $('#delete-switch').on(
-    'click',
-    function(e) {
-      e.preventDefault();
-      var delete_ip = prompt('What is the IP address for the switch to delete?');
-      var delete_label = prompt('What is the name of this switch to delete?');
-
-      var delete_switch = {label: delete_label, ip: delete_ip};
-      var delete_index = -1;
-      for ( var i = 0; i < saved_switches.length; i++ ) {
-        if ( saved_switches[i].ip == delete_switch.ip && saved_switches[i].label == delete_switch.label ) {
-          delete_index = i;
-        }
-      }
-      if ( delete_index > -1 ) {
-        saved_switches.splice( delete_index, 1 );
-      }
-
-      $.jStorage.set('switches',saved_switches);
-
-      $('li[data-ip="' + delete_switch.ip + '"]').remove();
-    }
-  );
+  var saved_switches = new Array();
 
   checkSwitches();
 })(jQuery);
